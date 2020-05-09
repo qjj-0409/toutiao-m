cd33f992c82b7e367803d51211d5080b4d2aacbe
qjj-0409 <qi18790396124@163.com>
2020-05-09 12:23:18 +0800

    项目初始化-导入Vant组件库

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import store from './store'
 // 引入全局样式文件
 import './styles/index.less'
 
+// 引入Vant组件库
+import Vant from 'vant'
+// 引入Vant组件库样式
+import 'vant/lib/index.css'
+
+// 全局注册Vant中的组件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
