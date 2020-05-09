657af39dd9f2aafb86835129baa66ac3ecad09b3
qjj-0409 <qi18790396124@163.com>
2020-05-09 19:14:22 +0800

    用户登录-基本功能实现

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,23 +1,98 @@
 <template>
   <div class="login-container">
-    登录页面
+    <van-nav-bar
+      class="app-nav-bar"
+      title="登录/注册"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- 登录表单 -->
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        icon-prefix="toutiao"
+        left-icon="shouji"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        clearable
+        icon-prefix="toutiao"
+        left-icon="yanzhengma"
+        placeholder="请输入验证码"
+      >
+        <template #button>
+          <van-button
+            class="send-btn"
+            size="mini"
+            round
+          >发送验证码</van-button>
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button
+          class="login-btn"
+          block
+          type="info"
+          @click="onlogin"
+        >登录</van-button>
+      </div>
+    </van-cell-group>
+    <!-- /登录表单 -->
   </div>
 </template>
 
 <script>
+import { login } from '@/api/user'
+
 export default {
   name: 'LoginIndex',
   props: {},
   components: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '', // 手机号
+        code: '' // 验证码
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
-  methods: {},
+  methods: {
+    async onlogin () {
+      try {
+        const res = await login(this.user)
+        console.log(res)
+      } catch (error) {
+        console.log('错误：' + error)
+      }
+    }
+  },
   mounted () {}
 }
 </script>
 <style lang='less' scoped>
+.login-container {
+  .send-btn {
+    width: 76px;
+    height: 23px;
+    background-color: #ededed;
+    .van-button__text {
+      color: #666666;
+      font-size: 11px;
+    }
+  }
+  .login-btn-wrap {
+    padding: 26px 16px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
+    .van-button__text {
+      font-size: 15px;
+    }
+  }
+}
 </style>
