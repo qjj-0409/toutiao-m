d85a42f9988517837d336a26ac59afe34956f5db
qjj-0409 <qi18790396124@163.com>
2020-05-14 13:32:55 +0800

    频道编辑-汉堡图标样式处理&控制弹出层显示

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home-container">
-    <!-- 导航栏 -->
+    <!-- 顶部导航栏 -->
     <van-nav-bar
       class="app-nav-bar"
     >
@@ -12,12 +12,26 @@
         size="small"
       >搜索</van-button>
     </van-nav-bar>
-    <!-- /导航栏 -->
+    <!-- /顶部导航栏 -->
     <!-- 文章频道列表 -->
     <!--
       通过v-model绑定当前激活标签对应的索引值，默认情况下启用第一个标签
     -->
-    <van-tabs v-model="active">
+    <van-tabs
+      v-model="active"
+      line-height="3"
+      line-width="15"
+      color="#3296fa"
+      class="channels-tabs"
+    >
+      <div class="tabs-placeholder" slot="nav-right"></div>
+      <div
+        class="tabs-right"
+        slot="nav-right"
+        @click="isPopupShow = true"
+      >
+        <van-icon name="wap-nav" />
+      </div>
       <van-tab
         v-for="channel in channels"
         :key="channel.id"
@@ -29,6 +43,16 @@
       </van-tab>
     </van-tabs>
     <!-- /文章频道列表 -->
+
+    <!-- 弹出层 -->
+    <van-popup
+      v-model="isPopupShow"
+      position="bottom"
+      :style="{ height: '100%' }"
+      closeable
+      close-icon-position="top-left"
+    ></van-popup>
+    <!-- /弹出层 -->
   </div>
 </template>
 
@@ -47,7 +71,8 @@ export default {
   data () {
     return {
       active: 0, // 控制激活的标签
-      channels: [] // 文章频道列表
+      channels: [], // 文章频道列表
+      isPopupShow: false // 控制是否显示弹出层
     }
   },
   computed: {},
@@ -82,6 +107,39 @@ export default {
         .van-button__text {
           font-size: 14px;
         }
+      }
+    }
+  }
+  .channels-tabs {
+    /deep/ .van-tab {
+      border-right: 1px solid #edeff3;
+      border-bottom: 1px solid #edeff3;
+    }
+    /deep/ .van-tabs__line {
+      bottom: 20px;
+    }
+    .tabs-placeholder {
+      width: 33px;
+      flex-shrink: 0;
+      border-bottom: 1px solid #edeff3;
+    }
+    .tabs-right {
+      position: fixed;
+      right: 0;
+      background-color: rgba(255,255,255,.9);
+      height: 42px;
+      width: 33px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      &::before {
+        content: '';
+        position: absolute;
+        left: 0;
+        width: 1px;
+        height: 29px;
+        background: url(./line.png) no-repeat;
+        background-size: cover;
       }
     }
   }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -73,7 +73,7 @@ export default {
   data () {
     return {
       user: {
-        mobile: '13911111111', // 手机号
+        mobile: '13922222222', // 手机号
         code: '246810' // 验证码
       },
       formRules: {
