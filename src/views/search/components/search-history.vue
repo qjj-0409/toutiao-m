81c68cf906bb337a0395eaadbf8b4bed4c493a87
qjj-0409 <qi18790396124@163.com>
2020-05-18 13:13:46 +0800

    文章搜索-删除所有历史记录

--- src/views/search/components/search-history.vue
+++ src/views/search/components/search-history.vue
@@ -4,7 +4,7 @@
       title="历史纪录"
     >
       <div v-if="isDeleteHistory">
-        <span @click="deleteAll">全部删除</span>
+        <span @click="$emit('update-histories', [])">全部删除</span>
         &nbsp;&nbsp;
         <span @click="isDeleteHistory = false">完成</span>
       </div>
@@ -29,9 +29,9 @@
 </template>
 
 <script>
-import { setItem } from '@/utils/storage'
+// import { setItem } from '@/utils/storage'
 import { mapState } from 'vuex'
-import { deleteSearchHistories } from '@/api/search'
+// import { deleteSearchHistories } from '@/api/search'
 
 export default {
   name: 'SearchHistory',
@@ -53,31 +53,33 @@ export default {
   watch: {},
   created () {},
   methods: {
-    async deleteHistory (history, index) {
+    deleteHistory (history, index) {
       if (this.isDeleteHistory) {
         // 如果是删除状态，执行删除操作
         this.searchHistories.splice(index, 1)
-        if (this.user) {
-          // 如果是登录状态，更新线上的搜索历史
-        } else {
-          // 如果未登录，删除本地的搜索历史
-          setItem('search-histories', this.searchHistories)
-        }
+        // if (this.user) {
+        //   // 如果是登录状态，更新线上的搜索历史
+        // } else {
+        //   // 如果未登录，删除本地的搜索历史
+        //   setItem('search-histories', this.searchHistories)
+        // }
+        // 不再判断用户是否登录，直接更新本地存储
+        // setItem('search-histories', this.searchHistories)
       } else {
         // 否则执行搜索操作
         this.$emit('search', history)
       }
-    },
-    async deleteAll () {
-      if (this.user) {
-        // 如果是登录状态，删除线上的所有
-        await deleteSearchHistories()
-      } else {
-        // 如果是未登录状态，则删除本地的数据，并将搜索历史更新为空
-        this.searchHistories = []
-        setItem('search-histories', null)
-      }
     }
+    // async deleteAll () {
+    //   if (this.user) {
+    //     // 如果是登录状态，删除线上的所有
+    //     await deleteSearchHistories()
+    //   } else {
+    //     // 如果是未登录状态，则删除本地的数据，并将搜索历史更新为空
+    //     this.searchHistories = []
+    //     setItem('search-histories', null)
+    //   }
+    // }
   },
   mounted () {}
 }
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -34,6 +34,7 @@
       v-else
       :search-histories="searchHistories"
       @search="onSearch"
+      @update-histories="searchHistories = $event"
     />
     <!-- /历史记录 -->
   </div>
@@ -44,7 +45,7 @@ import SearchSuggestion from './components/search-suggestion'
 import SearchHistory from './components/search-history'
 import SearchResult from './components/search-result'
 import { setItem, getItem } from '@/utils/storage'
-import { getSearchHistories } from '@/api/search'
+// import { getSearchHistories } from '@/api/search'
 import { mapState } from 'vuex'
 
 export default {
@@ -65,7 +66,12 @@ export default {
   computed: {
     ...mapState(['user'])
   },
-  watch: {},
+  watch: {
+    // 在watch中监听数据变化，实现持久化
+    searchHistories () {
+      setItem('search-histories', this.searchHistories)
+    }
+  },
   created () {
     this.loadSearchHistories()
   },
@@ -87,27 +93,31 @@ export default {
       // 如果用户已登录，则把搜索历史记录存储到线上
       // 提示：只要我们调用获取搜索结果的数据接口，后端会给我们自动存储用户的搜索历史纪录
       // 如果没有登录，则把搜索历史纪录存储到本地
-      setItem('search-histories', this.searchHistories)
+      // setItem('search-histories', this.searchHistories)
 
       // 展示搜索结果
       this.isResultShow = true
     },
     async loadSearchHistories () {
       // 获取用户搜索历史纪录
-      // 因为后端存储的用户搜索历史记录只有4条，所以这里为了让历史记录看起来多一些，所以将本地的历史记录和后端存储的历史记录合并在一起
-      let searchHistories = getItem('search-histories') || []
-      // 如果用户已登录
-      if (this.user) {
-        // 获取用户后端存储的搜索历史记录
-        const { data } = await getSearchHistories()
-        // searchHistories = data.data.keywords
-        // 合并本地和线上的搜索历史纪录
-        searchHistories = [...new Set([
-          ...searchHistories,
-          ...data.data.keywords
-        ])]
-      }
-      // 赋值给该组件中存放历史记录的数据
+      // // 因为后端存储的用户搜索历史记录只有4条，所以这里为了让历史记录看起来多一些，所以将本地的历史记录和后端存储的历史记录合并在一起
+      // let searchHistories = getItem('search-histories') || []
+      // // 如果用户已登录
+      // if (this.user) {
+      //   // 获取用户后端存储的搜索历史记录
+      //   const { data } = await getSearchHistories()
+      //   // searchHistories = data.data.keywords
+      //   // 合并本地和线上的搜索历史纪录
+      //   searchHistories = [...new Set([
+      //     ...searchHistories,
+      //     ...data.data.keywords
+      //   ])]
+      // }
+      // // 赋值给该组件中存放历史记录的数据
+      // this.searchHistories = searchHistories
+
+      // 不再获取后端的用户搜索历史记录，直接使用本地的
+      const searchHistories = getItem('search-histories') || []
       this.searchHistories = searchHistories
     }
   },
