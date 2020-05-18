46727db87ed245e8e38b310cf2ce471b7704cfc9
qjj-0409 <qi18790396124@163.com>
2020-05-18 21:12:29 +0800

    文章详情-图片预览

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -33,6 +33,7 @@
     <div
       class="markdown-body"
       v-html="article.content"
+      ref="article-content"
     >
     </div>
   </div>
@@ -41,6 +42,7 @@
 <script>
 import './github-markdown.css'
 import { getArticleById } from '@/api/article'
+import { ImagePreview } from 'vant'
 
 export default {
   name: 'ArticleIndex',
@@ -67,6 +69,34 @@ export default {
       const { data } = await getArticleById(this.articleId)
       console.log(data)
       this.article = data.data
+      // 图片预览
+      // 数据改变影响视图更新（DOM不是立即的）
+      this.$nextTick(() => {
+        this.handlePreviewImage()
+      })
+    },
+    // 图片预览
+    handlePreviewImage () {
+      // 1.获取文章内容的DOM容器
+      const articleContent = this.$refs['article-content']
+      // 2.得到所有的img标签
+      const imgs = articleContent.querySelectorAll('img')
+      // console.log(imgs) 控制台打印结果：NodeList[]
+      // 原因是：数据改变影响视图更新（DOM数据）不是立即的，如果需要在修改数据之后，马上操作该数据影响的视图DOM，需要把这个代码放到$nextTick()中
+      // $nextTick()是Vue提供的一个方法
+
+      // 3.循环img列表，给img注册点击事件（记录每一张图片的路径，放在一个数组中）
+      const imgPaths = []
+      imgs.forEach((img, index) => {
+        imgPaths.push(img.src)
+        img.onclick = function () {
+          // 4.在事件处理函数中调用ImagePreview()预览
+          ImagePreview({
+            images: imgPaths, // 预览图片路径列表
+            startPosition: index // 起始位置
+          })
+        }
+      })
     }
   },
   mounted () {}
